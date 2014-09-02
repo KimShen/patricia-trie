6eee7ab761979b4ee549448506dfa2344d40274f
kim.shen <sjw_job@126.com>
2014-09-02 18:07:47 +0800

    Implement Trie Filter for special words

---
+++ src/test/java/org/ardverk/filter/SimpleTrieFilterText.java
@@ -0,0 +1,42 @@
+package org.ardverk.filter;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.IOUtils;
+import org.ardverk.filter.impl.SimpleTrieFilter;
+import org.junit.Test;
+
+/**
+ * @author kim 2014年9月2日
+ */
+public class SimpleTrieFilterText {
+
+	private final Map<String, String> trie = new HashMap<String, String>();
+
+	private final String content;
+
+	public SimpleTrieFilterText() throws Exception {
+		super();
+		this.content = IOUtils.toString(SimpleTrieFilterText.class.getResourceAsStream("War and Peace.txt"), "UTF-8");
+		for (String each : IOUtils.readLines(new FileReader(new File(SimpleTrieFilterText.class.getResource("words.txt").getFile())))) {
+			this.trie.put(each.trim(), "*");
+		}
+	}
+
+	@Test
+	public void testFilter() throws Exception {
+		TrieCounter after = new SimpleTrieFilter(this.trie).filter(this.content);
+		int total = 0;
+		for (int index = 0; index != -1; total++) {
+			index = after.source().indexOf("*", index + 1);
+		}
+		TestCase.assertSame(16, total);
+		TestCase.assertSame(16, after.filtered());
+		TestCase.assertTrue(after.source().length() < this.content.length());
+	}
+}
